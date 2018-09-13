43b5aed2e9e0e44a8880271c492fb4262ff25d64
Chuang Sun <csun@yitu-inc.intra>
2018-09-13 14:56:27 +0800

    2.2.4在活动中使用Toast

--- ActivityTest/app/src/main/java/activitytest/example/com/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/activitytest/example/com/activitytest/FirstActivity.java
@@ -12,6 +12,7 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        // findViewById 方法返回一个View对象，向下转型为Button对象，通过调用setOnClickListener为按钮注册一个监听器，点击按钮则会执行监听器中的onClick()方法。
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
