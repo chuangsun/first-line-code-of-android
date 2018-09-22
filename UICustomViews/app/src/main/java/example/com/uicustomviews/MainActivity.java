8bd920dfa30723adcffebd3b5e31c72900c24ef9
Chuang Sun <csun@yitu-inc.intra>
2018-09-22 12:25:37 +0800

    自定义控件和布局

---
+++ UICustomViews/app/src/main/java/example/com/uicustomviews/MainActivity.java
@@ -0,0 +1,18 @@
+package example.com.uicustomviews;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+    }
+}
